db1dbafe94f5e58add88534c0990ee12f4830bd2
tiila <tiila.kaenniemi@gmail.com>
2018-12-09 08:26:52 +0200

    #11 #Adding a test object for Solr indexing and correcting some variable names

--- email_combiner.py
+++ email_combiner.py
@@ -2,18 +2,18 @@
 # Combining the information into a list of dictionaries
 # Indexing data to Solr
 
-# import pysolr
+import pysolr
 import json
 import os
 import re
 
 # Setting up Solr, note the port number
-# solr = pysolr.Solr('http://localhost:8983/solr/', timeout=10, auth= < type of authentication >)
+solr = pysolr.Solr('http://localhost:8983/solr/email', timeout=10)
 
 # Making a list for all email dictionaries
 emails = []
 subject = "subject"
-body = "body"
+text = "body"
 combinations = {}
 
 json_postit = "./json_postit/"
@@ -22,18 +22,19 @@ emails = {}
 
 # json_file in with is and file object, file handle
 for filename in os.listdir(json_postit):
-    file_number = re.search('^[0-9]+', filename)  # regex
+    file_number = re.search('^[0-9]+', filename)
     json_postit_filename = json_postit + filename
     if os.path.isfile(json_postit_filename) \
             and filename.endswith(".json") \
-            and not filename in json_files:
+            and not filename in json_postit:
         with open(json_postit_filename, encoding="utf-8") as json_file:
             data = json.load(json_file)
             # Finding the value of the subject field from the json file
             subject_name = data["subject"]
             # Combining the subject and body values into an email dictionary
             email = {'subject': subject_name}
-            emails = {file_number: email}
+           # emails = {file_number: email}
+            emails['subject']=subject_name
             # Appending the email values to the list
 
 print(emails)
@@ -49,19 +50,22 @@ for filename in os.listdir(text_postit):
     text_postit_filename = text_postit + filename
     if os.path.isfile(text_postit_filename) \
             and filename.endswith(".txt") \
-            and not filename in txt_files:
+            and not filename in text_postit:
         with open(text_postit_filename, "r", encoding="utf-8") as txt_file:
             body_text = txt_file.read()
             if file_number in emails:
                 email = emails[file_number]
-                email['body'] = body_text
+                email['text'] = body_text
                 emails[email] = email
-
-            else
-                email_body = {'body': body_text}
+            else:
+                email_body = {'text': body_text}
                 emails = {file_number: email_body}
 
-print(bodies[0])
+print(emails)
 
 # Indexing emails to solr
-# solr.add(emails)
+solr.add([
+    {
+        "subject": "Testi",
+        "text": "Testi_body"}
+])
