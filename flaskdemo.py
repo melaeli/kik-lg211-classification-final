e6166cbe0ba814f2a659a9ac38c7c91710644ae6
melaeli <elina.melasuo@helsinki.fi>
2018-12-29 00:00:31 +0200

    #18 The Python file that makes the Flask to be able to extract data from Solr and show it in UI. Situation 28.12.2018.

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,53 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from urllib.request import *
 
-#selaimeen: http://localhost:8000/solr/
-#lisätty wikiin from urllib.request import *
-#huom.Dimalla from urllib, joka toimii Diman esimerkkiin: https://github.com/DmitryKey/kik-lg211-2018/blob/master/flask-solr/flaskdemo.py
-#Mathiaksella from urllib.request, joka toimii toiseen wiki-vaihtoehtoon ks. flaskdemo_wiki_dima.py - jälkimmäisellä toimii http://localhost:8000/search
-
+#Open Solr and Flask in localhosts 8000/8983 and put into browser: http://localhost:8000/solr/ and http://localhost:8983/solr - core email
 
 #Initialize Flask instance
 app = Flask(__name__)
 
-example_data = [
-    {"name": "Cat sleeping on a bed", "source": "cat.jpg"},
-    {"name": "Misty forest", "source": "forest.jpg"},
-    {"name": "Bonfire burning", "source": "fire.jpg"},
-    {"name": "Old library", "source": "library.jpg"},
-    {"name": "Sliced orange", "source": "orange.jpg"}
-]
 
-#Use "query" variable from the URL. If no variable is given,
-#use empty string instead. GET and POST methods are allowed.
-@app.route("/search", defaults={"query": ""}, methods=["GET", "POST"])
-@app.route("/search/<query>", methods=["GET", "POST"])
-def search(query):
-
-    if request.method == "POST":
-        #Get query from the POST form.
-        query = request.form["query"]
-
-        #Redirect to the same page with the query in the url.
-        #ALWAYS REDIRECT AFTER POSTING!
-        return redirect(url_for("search", query=query))
-
-    matches = []
-
-    #If an entry name contains the query, add the entry to matches.
-    if query != "":
-        for entry in example_data:
-            if query.lower() in entry["name"].lower():
-                matches.append(entry)
-
-    #Render index.html with matches variable.
-    return render_template("index.html", matches=matches)
+#Copied from the code of Otto and Juuso, thank you guys!
+#€ does not get an error code but is not included to the search after all
+#Replaces whitespaces with a plus and fixes the ascii problem with ääkköset in the query (åäöüÅÄÖÜ) and with euro sign
+def addplusaakkoset(query):
+    fixed = query.replace(" ", "+").replace("ä", "%C3%A4").replace("ö", "%C3%B6").replace("å", "%C3%A5").replace("ü", "%C3%BC").replace("Ü", "%C3%9C").replace("Ä", "%C3%84").replace("Ö", "%C3%96").replace("Å", "%C3%85").replace("€", "%E2%82%AC")
+    return fixed
 
 
-#miksi defaults={"query": "cat"} in https://github.com/DmitryKey/kik-lg211-2018/blob/master/flask-solr/flaskdemo.py - KOSKA URL?
-#JOS defaults={"query": ""} kuten in def(search) ja Mathiaksen esimerkillä niin antaa ERROR: urllib.error.HTTPError: HTTP Error 400: Bad Request
-#JOS defaults={"query": "Testi"} niin antaa tyhjän aloitussivun; JOS defaults={"query": "Testi_body"} niin antaa hakutuloksen jo päivittäessä sivun - mitä tähän kuuluisi laittaa?
-@app.route("/solr/", defaults={"query": "Testi"}, methods=["GET", "POST"])  #Dimalla "/solr", joka antoi errorin: Not Found  
+#Exctracting emails subject/text instead of wiki documents title/text (done in class) or pictures in def search(query) (in example demo)
+#defaults={"query": "This_is_not_Supposed_to_Match_Anything"} works -> this is changed into an empty string which finally worked with ""
+#Use "query" variable from the URL. If no variable is given, use empty string instead. GET and POST methods are allowed.
+@app.route("/solr/", defaults={"query": '""'}, methods=["GET", "POST"])
 @app.route("/solr/<query>", methods=["GET", "POST"])
 def solr(query):
     if request.method == "POST":
@@ -59,73 +30,58 @@ def solr(query):
         return redirect(url_for("solr", query=query))
 
     matches = []
+    count=0
 
-#laita näin omaan demoon kuten Dimalla:
-#    url = 'http://localhost:8983/solr/wiki/select?q=text:' + query + '&wt=python&start=0&rows=10&fl=title,id,text&hl=on&hl.fl=text&hl.simple.post=</b>&hl.simple.pre=<b>'
-#    print(url)
-#    connection = urlopen(url)
+    # Some of the common words like JA and OVAT don't work in the search because Solr stemms them into nothing
+    # Search both from the subject and the text including SPACE
+    # Searches the words separately without "" which is instructed in the HTML file; juhla NOT talven search works, too
+    url = 'http://localhost:8983/solr/email/select?q=(subject:(' + addplusaakkoset(
+        query) + ')+OR+text:(' + addplusaakkoset(query) + '))&wt=python&start=0' + \
+          '&rows=100&fl=text,subject,id&hl=on&hl.fl=text&hl.simple.post=</b>&hl.simple.pre=<b>'
 
-#Mathiaksella:
-#    connection = urlopen('http://localhost:8983/solr/wiki/select?q=text:cheese&wt=python&start=0&rows=100')
+    # Trying to add highlighting with Mathias but didn't get it to work (hl.method, hl.fraqsize); leaving the code here anyways for the possible development
+    #    url = 'http://localhost:8983/solr/email/select?q=(subject:(' + addplusaakkoset(query) + ')+OR+text:(' + addplusaakkoset(query) + '))&wt=python&start=0&rows=100&$
 
-#HUOM. text:Testi_body PITÄÄ OLLA TÄYSIN SAMASSA MUODOSSA, pelkkä text:testi ei löydä mitään!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Korjaa?
-#    connection = urlopen('http://localhost:8983/solr/email/select?q=text:Testi_body&wt=python&start=0&rows=100')
-
-#    connection = urlopen('http://localhost:8983/solr/email/select?q=text:' + query + '&wt=python&start=0&rows=100') # defaults={"query": ""}
-#ERROR: urllib.error.HTTPError 400: Bad Request
-
-#    connection = urlopen('http://localhost:8983/solr/email/select?q=text:' + query + '&wt=python&start=0&rows=10&fl=subject,text&hl=on&hl.fl=text&hl.simple.post=</b>&hl.simple.pre=<$'
-#ERROR: response = eval(connection.read())  SyntaxError: invalid syntax
-#           ^
-
-#hakusana löytää vain tekstikentän eli hakusanalla Teksti_body (ei tulosta, jos hakee Testi, vaikka se on subjektina) - miten löytäisi myös subjectilla???
-    url = 'http://localhost:8983/solr/email/select?q=text:' + query + '&wt=python&start=0&rows=10'
-#    url = 'http://localhost:8983/solr/email/select?q=text:Testi_body&wt=python&start=0&rows=100'
-#urllib.error.HTTPError: HTTP Error 400: Bad Request
     print(url)
     connection = urlopen(url)
 
     response = eval(connection.read())
 
+    #A little bit confusing because having the same name: response['response']...
     print(response['response']['numFound'], "documents found.")
 
-###ERROR in Dima's code: missing parentheses
-#    print ("=========================")
 #    print (response['highlighting'])
-#    print ("=========================")
-###
 
-    # Print the name of each document.
-
+    #Print the name of each document. This is shown in the shell where the Flask is opened like the print(url) and the for loop below and the print (count).
     print("Printing 100 top documents:")
 
+
     for i, document in enumerate(response['response']['docs']):
-        print(" Email subject ", i, "=", document['subject'])
-
-#tai kuten Dimalla:
-#        # print("  Document title ", i, "=", document['title'])
-#        id = document['id']
-#        print ("id=" + id)
-#        # print(response['response']['highlighting'][id])
-#        highlight = response['highlighting'][id]['text'][0]
-#        match = {"title": document['title'], "text": highlight}
+        print(" Email subject ", i, "=", i+1, document['subject'])
 
 
-#Exctracting emails einstead of wiki documents title/text or pictures in def search(query)
-#OUR PROJECT: solr schemaan subject (done), jotta saadaan ['subject'] tähän:
-        match = {"subject": document['subject'], "text": document['text']}
+#Testing highlighting: gave html tags without bolding the text itself and only the row where the (first) bolding word occurred - not used in the programme now
+#Checked from Solr q, fl, hl, wt etc. but didn't get it work here, needs more investigating...
+#To make higlighting to work, the subject and text must be retrieved from different places to the matches
+#ID is propably giving by Solr and must be included with highlighting
+        # print("  Document title ", i, "=", document['title'])
+        id = document['id']
+        print ("id=" + id)
+        if len(response['highlighting'][id])==0:
+            highlight = document['text']
+        else:
+            highlight = response['highlighting'][id]['text'][0]
+
+
+#Subject field put into the Solr Schema file; core email done in Solr, no need for input file/folder as the program takes the files there one by one; no need for han$
+#        match = {"subject": document['subject'], "text": highlight}
+
+        #Adding the search result count in front of every result in UI
+        count=count+1
+        match = {"count": count, "subject": document['subject'], "text": document['text']}
+
         matches.append(match)
+        print (count)
 
     #Render index.html with matches variable.
-    return render_template("index.html", matches=matches)
-#tai solr.html, jos teet sennimisen uuden templaten, tai minulla ks. templates/index_wiki.html
-
-
-#Core email tehty (ei tarvetta input kansiolle, koska meidän oma ohjelma vie sinne yksitellen) ja kopioitu schema wikistä ja muutettu fieldit meille sopivaksi
-#pysolr toimimaan pycharmiin done 7.12.2018
-#TODO:
-#form solr objects: iterointi, jossa käydään dictionaryt läpi ja luodaan solr objektit, jotka listassa, joka lisätään solr.add
-#(filenro dict, jonka alla dict, jossa subj ja body - huom vaihda body text:ksi, jotta field löytää
-#solr.add sis. subj ja text
-#lisää themes extraction, ennakoiva syöttö...
-#KYSYMYS: Miten Flask tajuaa, että juuri flaskdemo.py käytössä eikä joku muu saman kansion .py?
+    return render_template("index.html", matches=matches, n=response['response']['numFound'])
